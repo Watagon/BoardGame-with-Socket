60632936f621311424e454e3461936cf928e4e04
watagon <waytagon@LAPTOP-TCJE96FA.localdomain>
2021-06-02 11:12:11 +0900

    Change variables name
    name conflict between connect4_front and connet
    not compile error but to make code more readable

--- connect4.c
+++ connect4.c
@@ -18,18 +18,18 @@ void new_game (Connect4_t *game, int col_num, int row_num)
 }
 
 static bool
-is_valid_move (Connect4_t *cnct4, int row, int col)
+is_valid_move (Connect4_t *game, int row, int col)
 {
-    assert(0<row && row<cnct4->row_num);
-    assert(0<col && col<cnct4->col_num);
+    assert(0<row && row<game->row_num);
+    assert(0<col && col<game->col_num);
 
-    if (row == cnct4->row_num-1)
+    if (row == game->row_num-1)
         return true;
     
-    uint64_t filled = cnct4->black|cnct4->white;
+    uint64_t filled = game->black|game->white;
     uint64_t empty  = ~filled;
-    uint64_t mask   = (uint64_t)1<<row*cnct4->col_num + col;
-    uint64_t below  = mask<<cnct4->col_num;
+    uint64_t mask   = (uint64_t)1<<row*game->col_num + col;
+    uint64_t below  = mask<<game->col_num;
 
     if (empty&mask && filled&below)
         return true;
@@ -38,21 +38,21 @@ is_valid_move (Connect4_t *cnct4, int row, int col)
 }
 
 int
-connect4_make_move (Connect4_t *cnct4, int row, int col)
+connect4_make_move (Connect4_t *game, int row, int col)
 {
-    if (!is_valid_move(cnct4, row, col))
+    if (!is_valid_move(game, row, col))
         return -1; 
     
-    uint64_t new_cell = (uint64_t)1<<row*cnct4->col_num+col;
+    uint64_t new_cell = (uint64_t)1<<row*game->col_num+col;
 
     uint64_t *cell_bits;
-    switch (cnct4->state)
+    switch (game->state)
     {
     case BLACK_MOVE:
-        cnct4->black |= new_cell;
+        game->black |= new_cell;
         break;
     case WHITE_MOVE:
-        cnct4->white |= new_cell;
+        game->white |= new_cell;
     default:
         break;
     }
@@ -62,13 +62,13 @@ connect4_make_move (Connect4_t *cnct4, int row, int col)
 
 
 Cell_state_t
-connect4_cell_state (Connect4_t *cnct4, int row, int col)
+connect4_cell_state (Connect4_t *game, int row, int col)
 {
-    uint64_t mask = (uint64_t)1<<row*cnct4->col_num + col;
+    uint64_t mask = (uint64_t)1<<row*game->col_num + col;
 
-    if (mask&cnct4->black)
+    if (mask&game->black)
         return CELL_BLACK;
-    else if (mask&cnct4->white)
+    else if (mask&game->white)
         return CELL_WHITE;
     else
         return CELL_EMPTY;
